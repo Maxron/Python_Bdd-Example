02f440402d47c92e6cf196bbf71a7c7f00fcbfee
Wei <zero.maxima@gmail.com>
2018-11-19 17:26:16 +0800

    [Add] Add step file snippets generated after run the "behave" command

---
+++ features/steps/step_fib.py
@@ -0,0 +1,13 @@
+@given(u'we have the number 10')
+def step_impl(context):
+    raise NotImplementedError(u'STEP: Given we have the number 10')
+
+
+@when(u'we calc the fib')
+def step_impl(context):
+    raise NotImplementedError(u'STEP: When we calc the fib')
+
+
+@then(u'we get the fib number 55')
+def step_impl(context):
+    raise NotImplementedError(u'STEP: Then we get the fib number 55')
