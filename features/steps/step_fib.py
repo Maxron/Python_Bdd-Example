5a78a5d6b9e7f275f548e734e79824dde98d73ef
Wei <zero.maxima@gmail.com>
2018-11-20 09:36:29 +0800

    [Update] Refactor test step method

--- features/steps/step_fib.py
+++ features/steps/step_fib.py
@@ -5,15 +5,15 @@ from app.fib import fibs
 STEPS:
 ---------------------------
 """
-@given(u'we have the number 10')
-def step_impl(context):
-    context.fib_number = 10
+@given(u'we have the number {number}')
+def step_impl(context, number):
+    context.fib_number = int(number)
 
 @when(u'we calc the fib')
 def step_impl(context):
     context.fib_number = list(fibs(context.fib_number))[-1]
 
-@then(u'we get the fib number 55')
-def step_impl(context):
-    context.expected_number = 55
+@then(u'we get the fib number {fib_number}')
+def step_impl(context, fib_number):
+    context.expected_number = int(fib_number)
     assert context.fib_number == context.expected_number, "Calc fib number: %d" % context.fib_number
